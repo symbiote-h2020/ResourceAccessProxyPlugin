a9e0e13f30269c31e407fc0bd68c9cbe113fbdc3
MarioKusek <mario.kusek@fer.hr>
2018-06-05 06:51:57 +0200

    Fixed rabbit configuration on other hosts

--- src/main/java/eu/h2020/symbiote/rapplugin/RapPluginConfiguration.java
+++ src/main/java/eu/h2020/symbiote/rapplugin/RapPluginConfiguration.java
@@ -7,6 +7,9 @@ import javax.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.beans.BeansException;
@@ -34,6 +37,36 @@ public class RapPluginConfiguration implements ApplicationContextAware {
     private static final Logger LOG = LoggerFactory.getLogger(RapPluginConfiguration.class);
     
     @Bean
+    public ConnectionFactory connectionFactory(RabbitConnectionProperties props) {
+        CachingConnectionFactory factory = new CachingConnectionFactory(props.getHost());
+        factory.setUsername(props.getUsername());
+        factory.setPassword(props.getPassword());
+		return factory;
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemaplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(new Jackson2JsonMessageConverter());
+        return rabbitTemplate;
+    }
+
+    @Bean
+    public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(ConnectionFactory connectionFactory) {
+        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory);
+        factory.setMessageConverter(new Jackson2JsonMessageConverter());
+        return factory;
+    }
+
+    @Bean
+    public RabbitAdmin amqpAdmin(ConnectionFactory connectionFactory) {
+        RabbitAdmin rabbitAdmin = new RabbitAdmin(connectionFactory);
+        rabbitAdmin.setIgnoreDeclarationExceptions(true);
+        return rabbitAdmin;
+    }
+    
+    @Bean
     public RabbitManager rapRabbitManager(RabbitTemplate template) {
     	return new RabbitManager(template);
     }
@@ -46,14 +79,6 @@ public class RapPluginConfiguration implements ApplicationContextAware {
     @Bean
     public RapPluginProperties rapPluginProperties(RabbitConnectionProperties rabbitConnection, RapProperties pluginProperties) {
     	return new RapPluginProperties(rabbitConnection, pluginProperties);
-    }
-    
-    @Autowired
-    private SimpleRabbitListenerContainerFactory simpleRabbitListenerContainerFactory;
-    
-    @PostConstruct
-    public void initialize() {
-        simpleRabbitListenerContainerFactory.setMessageConverter(new Jackson2JsonMessageConverter());
     }
     
     @Override
