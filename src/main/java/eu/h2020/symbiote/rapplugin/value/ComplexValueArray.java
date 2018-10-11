7706f7118af2ebb1ebf739fc06cf3b926f3b9290
Jacoby <michael.jacoby@iosb.fraunhofer.de>
2018-10-11 15:02:14 +0200

    adding support for arrays of complex types

---
+++ src/main/java/eu/h2020/symbiote/rapplugin/value/ComplexValueArray.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eu.h2020.symbiote.rapplugin.value;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Michael Jacoby <michael.jacoby@iosb.fraunhofer.de>
+ */
+public class ComplexValueArray implements Value<List<Value>> {
+
+    List<Value> values;
+    
+    public ComplexValueArray() {
+        this.values = new ArrayList<>();
+    }
+
+    public ComplexValueArray(List<Value> values) {
+        this.values = values;
+    }
+    
+    @Override
+    public boolean isComplex() {
+        return true;
+    }
+
+    @Override
+    public List<Value> get() {
+        return values;
+    }    
+}
